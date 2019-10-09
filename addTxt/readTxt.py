0b2dca94d21b750b4c0dad94842b29e7ae05dbbd
韩星星 <myhanguoxing@126.com>
2019-10-09 12:16:18 +0800

    添加脚本readtxt

---
+++ addTxt/readTxt.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+# 把目录下的txt合并成一个txt
+import os
+import time
+
+date = str(time.strftime('%Y-%m-%d',time.localtime()))
+
+path = os.path.join(input("请输入需要爬取的路径："))
+# 获取列表
+filenames = os.listdir(path)
+# 写入
+writeFile = open("F:\\WorkTools\\result\\"+date+"-result.txt", 'w', encoding='utf8')
+
+for filename in filenames:
+    print(filename+" 写入中....")
+    for line in open(path+"\\"+filename, encoding='utf8', errors='ignore'):
+        writeFile.writelines(line)
